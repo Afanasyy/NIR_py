24d5cbaef5de505d2b733ec4ff0c8152384af02c
Afanasyy <gri.kirill2005@yandex.ru>
2022-02-21 23:18:56 +0300

    fix

--- PythonApplication2.py
+++ PythonApplication2.py
@@ -1,36 +1,66 @@
 import requests, xmltodict, json, sqlite3
 from OSMPythonTools.api import Api
+import time
 
 
-key=""
+def re(url, retry=2):
+    try:
+        r = requests.get(url)
+        assert (r.status_code == 200), ("Ошибка, Код ответа: ", r.status_code, url)
+    except Exception as _e:
+        if retry:
+            print('retry=', retry, url)
+            return re(url, retry=(retry - 1))  # retry fail
+        else:
+            raise  # fail
+    else:
+        return r  # ok
+
+key_G="AIzaSyDrcM0eeiZE9cE-seAFvdsnc8iDBP8FvYk"
+key_Y="7626e5f6-f640-43ef-b927-469881d57da7"
 url3 = "https://api.openstreetmap.org/api/0.6/"
 payload={}
 headers = {}
+con = sqlite3.connect('db2.db')
+cur = con.cursor()
 
 with open("school.json", "r", encoding='utf-8') as read_file:
     school = json.load(read_file)
 
+
+
+s_addr=[]
 arr=[]
+ind=1;
+s='CREATE TABLE if not exists schools(id text,addr text, cord text)'
+#cur.execute(s)
+s="delete from schools"
+#cur.execute(s)
+s='INSERT INTO schools VALUES (?, ?, ?)'
 for i in school["elements"]:
     if i["type"]=="node":
         break
-    addr=i["tags"]["addr:street"].replace(' ', '+')+','+i["tags"]["addr:housenumber"]+','+i["tags"]["addr:postcode"]
-    url2="https://maps.googleapis.com/maps/api/geocode/json?address="+addr+"&key="+key
+    addr="Нижний Новгород, "+i["tags"]["addr:street"]+', '+i["tags"]["addr:housenumber"]+', '+i["tags"]["addr:postcode"]
+    url2="https://maps.googleapis.com/maps/api/geocode/json?address="+addr+"&key="+key_G
     response = requests.request("GET", url2 , headers=headers, data=payload)
     tmp=json.loads(response.text)
-    cord=(tmp["results"][0]["geometry"]["location"]["lat"],tmp["results"][0]["geometry"]["location"]["lng"])
-    print(cord)
-    arr.append(cord)
+    cord=str(tmp["results"][0]["geometry"]["location"]["lat"])+','+str(tmp["results"][0]["geometry"]["location"]["lng"])
+    print(addr,' / ', cord)
+    arr.append(['s'+str(ind),cord])
+    tmp=('s'+str(ind),addr,cord)
+    s_addr.append(tmp)
+    cur.execute(s,tmp)
+    ind+=1
 read_file.close()
 print("###################################################")
-con = sqlite3.connect('db.db')
-
-cur = con.cursor()
-s='CREATE TABLE if not exists final(lat DOUBLE not null, lon DOUBLE not null'
+con.commit()
+s='CREATE TABLE if not exists final(cord text'
 for i in arr:
-    s+=',"'+str(i[0])+' '+str(i[1])+'" DOUBLE not null'
-s+=',?)'
-cur.execute(s)
+    s+=',"'+i[0]+'" text not null'
+s+=',addr text)'
+#cur.execute(s)
+s="delete from final"
+#cur.execute(s)
 
 
 
@@ -62,26 +92,25 @@ for i in range(count, max_len):
             count+=1
             continue
      
-     url2="https://maps.googleapis.com/maps/api/geocode/json?address="+addr+"&key="+key
+     url2="https://maps.googleapis.com/maps/api/geocode/json?address="+addr+"&key="+key_G
      res = requests.request("GET", url2 , headers=headers, data=payload)
      tmp=json.loads(res.text)
-     cord=[tmp["results"][0]["geometry"]["location"]["lat"],tmp["results"][0]["geometry"]["location"]["lng"]]
+     cord=str(tmp["results"][0]["geometry"]["location"]["lat"])+','+str(tmp["results"][0]["geometry"]["location"]["lng"])
        
      file=open("text.txt", "w")
      file.write(str(count))
      file.close()
-     A=cord
      l=[]
      for i in arr:
-        B=i
-        url1 = "https://maps.googleapis.com/maps/api/directions/json?origin="+str(A[0])+"%2C"+str(A[1])+"&destination="+str(B[0])+"%2C"+str(B[1])+"&mode=walking&key="+key
-        response = requests.request("GET", url1 , headers=headers, data=payload)
+        url1 = "https://api.routing.yandex.net/v2/route?apikey="+key_Y+"&waypoints="+cord+"|"+i[1]+"&mode=walking"
+        response = re(url1)
         tmp=json.loads(response.text)        
-        l.append(tmp["routes"][0]["legs"][0]["distance"]["value"])
+        l.append(tmp['route']['legs'][0]['steps'][0]['length'])
      count+=1
-     tmp=cord + l + addr     
+     addr=addr.replace('+', ' ')
+     tmp2=[cord]+ l+ [addr]    
      print(cord)  
      s=len(l)*(",?")
-     cur.execute("INSERT INTO final VALUES (?,?"+s+',?)',tmp)
+     cur.execute("INSERT INTO final VALUES (?"+s+',?)',tmp2)
      con.commit()
     
